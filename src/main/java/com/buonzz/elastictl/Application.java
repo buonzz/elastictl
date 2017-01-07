adaeb2bab46c641c12410ce0adbc18cf8ed71f4e
buonzz <buonzz@gmail.com>
2017-01-08 01:22:08 +0800

    list all indices

--- src/main/java/com/buonzz/elastictl/Application.java
+++ src/main/java/com/buonzz/elastictl/Application.java
@@ -4,11 +4,14 @@ import javax.swing.*;
 import com.buonzz.elastictl.ui.*;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.JestResult;
 import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.indices.aliases.GetAliases;
+import java.io.IOException;
 
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
             FrameBuilder frameBuilder = new FrameBuilder();
 	    JFrame f = frameBuilder.build();
@@ -17,12 +20,29 @@ public class Application {
 	     f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
              f.setVisible(true);//making the frame visible  
              
-             JestClientFactory factory = new JestClientFactory();
-            factory.setHttpClientConfig(new HttpClientConfig
-                                   .Builder("http://localhost:9200")
-                                   .multiThreaded(true)
-                                   .build());
-            JestClient client = factory.getObject();
+            HttpClientConfig config;
+            JestClientFactory factory;
+            JestClient client;
+            GetAliases aliases;
+            JestResult result;
+            String json;
+
+            config = new HttpClientConfig.
+               Builder("localhost:9200").
+               build();
+
+            aliases = new GetAliases.
+               Builder().
+               build();
+
+            factory = new JestClientFactory();
+
+            factory.setHttpClientConfig(config);
+
+            client = factory.getObject();
+            result = client.execute(aliases);
+            json   = result.getJsonString();
+            System.out.print(json);
 
         }
 
