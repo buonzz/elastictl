30744429569da917ea64f5c08ba09dbbe5121e54
buonzz <buonzz@gmail.com>
2017-01-08 01:18:49 +0800

    used jest client

--- src/main/java/com/buonzz/elastictl/Application.java
+++ src/main/java/com/buonzz/elastictl/Application.java
@@ -2,6 +2,9 @@ package com.buonzz.elastictl;
 
 import javax.swing.*;
 import com.buonzz.elastictl.ui.*;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
 
 public class Application {
 
@@ -13,6 +16,14 @@ public class Application {
 	     f.setExtendedState(f.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 	     f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
              f.setVisible(true);//making the frame visible  
+             
+             JestClientFactory factory = new JestClientFactory();
+            factory.setHttpClientConfig(new HttpClientConfig
+                                   .Builder("http://localhost:9200")
+                                   .multiThreaded(true)
+                                   .build());
+            JestClient client = factory.getObject();
+
         }
 
 }
