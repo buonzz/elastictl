09de0a52fbe3db465f35c19a7f433e41b00dd437
buonzz <buonzz@gmail.com>
2017-02-19 17:51:51 +0800

    index listing

---
+++ src/Commands/Indices/ListIndicesCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Buonzz\Elastictl\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Buonzz\Elastictl\ClientFactory;
+
+class ListIndicesCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('indices:list')
+            ->setDescription('List Indices');
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $client = ClientFactory::getClient(); 
+        $response = $client->cluster()->stats();
+        $output->writeln("<comment>Host:</comment> " . getenv("ES_HOST"));
+    }
+}
