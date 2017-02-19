81a74dd11bcb36ff4dd23e89f8d4e1f9183d7761
buonzz <buonzz@gmail.com>
2017-02-19 17:27:17 +0800

    added info

--- src/Commands/ClusterInfoCommand.php
+++ src/Commands/ClusterInfoCommand.php
@@ -24,6 +24,15 @@ class ClusterInfoCommand extends Command
 
         $client = ClientFactory::getClient(); 
         $response = $client->cluster()->stats();
-        $output->writeln($response);
+
+        $output->writeln("<comment>Host:</comment> " . getenv("ES_HOST"));
+        $output->writeln("<comment>Cluster Name:</comment> " . $response['cluster_name']);
+        $output->writeln("<comment>Status:</comment> " . $response['status']);
+        $output->writeln("<comment>Indices Count:</comment> " . $response['indices']['count']);        
+        $output->writeln("<comment>Documents Count:</comment> " . $response['indices']['docs']['count']);
+        $output->writeln("<comment>Nodes Count:</comment> " . $response['nodes']['count']['total']);
+        $output->writeln("<comment>Versions:</comment> " 
+                    . implode(",", $response['nodes']['versions'])
+                );
     }
 }
