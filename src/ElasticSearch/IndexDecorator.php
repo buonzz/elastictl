979f28f50ac6348122788ff3b382296f40f6ee87
buonzz <buonzz@gmail.com>
2017-03-12 01:22:13 +0800

    format results

--- src/ElasticSearch/IndexDecorator.php
+++ src/ElasticSearch/IndexDecorator.php
@@ -6,6 +6,7 @@ use Buonzz\Elastictl\ElasticSearch\ClientFactory;
 class IndexDecorator{
 
 	private $output;
+	public $format_result=true;
 	
 	public function __construct($indexname){
 
@@ -16,14 +17,32 @@ class IndexDecorator{
 		$health = $client->cluster()->health(['index' => $indexname, 'level' => 'shards']);
 
 		$output['name'] = $indexname;
-		$output['documents'] = $stats['indices'][$indexname]['total']['docs']['count']; 
-		$output['size'] = $stats['indices'][$indexname]['total']['store']['size_in_bytes'];
 		$output['health'] = $health['indices'][$indexname]['status'];
+
+		if($format_result)
+		{
+			$output['documents'] = $stats['indices'][$indexname]['total']['docs']['count']; 
+			$output['size'] = $stats['indices'][$indexname]['total']['store']['size_in_bytes'];
+		}
+		else
+		{
+			$output['documents'] = $this->formatBytes($stats['indices'][$indexname]['total']['docs']['count']); 
+			$output['size'] = strpad(number_format($stats['indices'][$indexname]['total']['store']['size_in_bytes']),11,STR_PAD_LEFT);			
+		}
 
 		$this->output = $output;		
 	}
 
 	public function get(){
 		return $this->output;
+	}
+
+
+	function formatBytes($size, $precision = 2)
+	{
+	    $base = log($size, 1024);
+	    $suffixes = array('', 'K', 'M', 'G', 'T');   
+
+	    return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
 	}
 }
