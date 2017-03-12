7ce677d6ea8ad401a748f08afd9230eb9a46ed4e
buonzz <buonzz@gmail.com>
2017-03-12 10:42:40 +0800

    added cache

---
+++ src/Cache.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Buonzz\Elastictl;
+
+class Cache{
+
+	private $m;
+
+	public function __construct(){
+		$this->m = new Memcached();
+		$this->m->addServer( getenv('MEMCACHED_SERVER') , 11211);
+	}
+
+	public function get($key){
+		return $this->m->get($key);
+	}
+
+	public function set($key, $value){
+		return $this->m->get($key, $value);
+	}
+
+	public function delete($key){
+
+	}
+}
--- src/ElasticSearch/IndexListing.php
+++ src/ElasticSearch/IndexListing.php
@@ -4,30 +4,54 @@ namespace Buonzz\Elastictl\ElasticSearch;
 use GuzzleHttp\Client;
 use Illuminate\Support\Collection;
 
+use Buonzz\Elastictl\Cache;
+
 class IndexListing{
 
 	private $client;
+	private $cache;
 	
 	public function __construct(){
+
+		$this->cache = new Cache;
 
 		$this->client = new Client([
 		    'base_uri' => getenv("ES_PROTOCOL") . '://'. getenv("ES_HOST") . ":". getenv("ES_PORT"),
 		    'timeout'  => getenv("ES_TIMEOUT"),
 		]);
+
 	} // constructor
 
 	public function get(){
-			$output = [];
+
+		$output = [];
+
+		$key = md5(getenv("ES_PROTOCOL") . '://'. getenv("ES_HOST") . ":". getenv("ES_PORT") . "Buonzz\Elastictl\IndexListing");
+
+		$value = $this->cache->get($key);
+
+		if($value == false)
+		{
+
 			$response = $this->client->request('GET', '*/_aliases');
 			if( $response->getStatusCode() == 200){
+
 				$body = $response->getBody();
 				$stringBody = (string) $body;
 				$data = json_decode($stringBody);
 				$output = array_keys(get_object_vars($data));
+
+				$this->cache->set($key, $output);
 			}
 
-			$col = new Collection($output);
-			return $col;
+		} // if 
+		else{
+			$output = $value;
+		}
+
+		$col = new Collection($output);
+		return $col;
+
 	}
 
 } // class
